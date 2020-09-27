b62377fe98e1a032647e8c504ec84f0456cf6915
Róbert Vári <mail.robertvari@gmail.com>
2020-09-27 13:07:15 +0200

    using context

--- pages/views.py
+++ pages/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render
 
+site_name = "Stylus Photography"
+
 
 def home_view(request):
-    return render(request, 'home.html')
+    context = {
+        "site_name": site_name,
+        "site_subtitle": "Ha már unod az egyforma képeket..."
+    }
+
+    return render(request, 'home.html', context)
 
 
 def gallery_view(request):
-    return render(request, 'gallery.html')
+    context = {
+        "site_name": site_name,
+        "site_subtitle": "Ha már unod az egyforma képeket..."
+    }
+
+    return render(request, 'gallery.html', context)
 
 
 def services_view(request):
-    return render(request, 'services.html')
+    context = {
+        "site_name": site_name,
+        "site_subtitle": "Ha már unod az egyforma képeket..."
+    }
+
+    return render(request, 'services.html', context)
 
 
 def about_view(request):
-    return render(request, 'about.html')
+    context = {
+        "site_name": site_name,
+        "site_subtitle": "Ha már unod az egyforma képeket..."
+    }
+
+    return render(request, 'about.html', context)
 
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    context = {
+        "site_name": site_name,
+        "site_subtitle": "Ha már unod az egyforma képeket..."
+    }
+
+    return render(request, 'contact.html', context)
