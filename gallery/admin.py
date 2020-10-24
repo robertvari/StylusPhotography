9e0be101f4f37254fba1b4415eaf3a4f604e5e48
Róbert Vári <mail.robertvari@gmail.com>
2020-10-24 10:29:19 +0200

    Detailed Photo admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 from .models import Category, Photo
 
 admin.site.register(Category)
-admin.site.register(Photo)
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "uploaded", "image"]
+    list_editable = ["category"]
+    search_fields = ["title"]
+    list_filter = ["category"]
+
+
+admin.site.register(Photo, PhotoAdmin)
