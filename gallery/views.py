6288d920684824a943493071dbd19659566ecc0d
Róbert Vári <mail.robertvari@gmail.com>
2020-10-24 12:08:07 +0200

    categories menu

--- gallery/views.py
+++ gallery/views.py
@@ -1,11 +1,15 @@
 from django.views.generic import ListView, DetailView
-from .models import Photo
+from .models import Photo, Category
 
 
 class GalleryView(ListView):
     model = Photo
     template_name = "gallery/gallery_home.html"
     context_object_name = "photos"
+
+    extra_context = {
+        "categories": Category.objects.order_by("title")
+    }
 
 
 class PhotoDetailsView(DetailView):
