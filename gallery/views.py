fa83b9702cf7cc6a4f89e4c78a425070093d87f3
Róbert Vári <mail.robertvari@gmail.com>
2020-10-24 12:48:01 +0200

    Get photos by categories

--- gallery/views.py
+++ gallery/views.py
@@ -11,6 +11,15 @@ class GalleryView(ListView):
         "categories": Category.objects.order_by("title")
     }
 
+    def get_queryset(self):
+        photos = Photo.objects.order_by('-uploaded')
+        category_name = self.request.GET.get("category")
+
+        if category_name:
+            photos = Photo.objects.filter(category__title=category_name)
+
+        return photos
+
 
 class PhotoDetailsView(DetailView):
     model = Photo
